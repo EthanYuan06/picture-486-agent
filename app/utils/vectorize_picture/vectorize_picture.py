761dbc14a12c7caef88afd13913078d10bd07602
EthanYuan <heyuanshi.dev@outlook.com>
2026-06-09 18:01:07 +0800

    将文本和图片URL以混合模态发送给大模型，去除手动融合导致特征丢失的问题

--- app/utils/vectorize_picture/vectorize_picture.py
+++ app/utils/vectorize_picture/vectorize_picture.py
@@ -1,11 +1,15 @@
 import os
-import numpy as np
-import pymysql
+
 import chromadb
+import pymysql
 from dashscope import MultiModalEmbedding
 
 # 配置
-DB_CONFIG = {"host": "localhost", "port": 3306, "user": "root", "password": "1234", "database": "486_picture",
+DB_CONFIG = {"host": "localhost",
+             "port": 3306,
+             "user": "root",
+             "password": "1234",
+             "database": "486_picture",
              "charset": "utf8mb4"}
 chroma_client = chromadb.PersistentClient("../../../chroma_data")
 coll = chroma_client.get_or_create_collection("picture_vectors")
@@ -17,25 +21,23 @@ def get_pics():
     with pymysql.connect(**DB_CONFIG) as conn:
         with conn.cursor(pymysql.cursors.DictCursor) as cur:
             cur.execute(
-                "SELECT id,name,introduction,category,tags,url FROM picture WHERE isDelete=0 AND reviewStatus=1 AND is_vectorized=0")
+                "SELECT "
+                "id,name,introduction,category,tags,url FROM picture "
+                "WHERE isDelete=0 AND reviewStatus=1 AND is_vectorized=0")
             return cur.fetchall()
 
 
 # 向量化
 def get_emb(url, text):
-    # 图片向量
-    img_emb = \
-    MultiModalEmbedding.call(model="qwen3-vl-embedding", input=[{"image": url}], api_key=API_KEY).output["embeddings"][
-        0]["embedding"]
-    # 文本向量
-    txt_emb = \
-    MultiModalEmbedding.call(model="qwen3-vl-embedding", input=[{"text": text}], api_key=API_KEY).output["embeddings"][
-        0]["embedding"]
-    # 融合
-    return ((np.array(img_emb) + np.array(txt_emb)) / 2).tolist()
+    resp = MultiModalEmbedding.call(
+        model="qwen3-vl-embedding",
+        input=[{"image": url, "text": text}],
+        api_key=API_KEY
+    )
+    return resp.output["embeddings"][0]["embedding"]
 
 
-# 标记已向量化
+# 向MySQL的picture表标记已向量化的数据
 def mark_done(pid):
     with pymysql.connect(**DB_CONFIG) as conn:
         conn.cursor().execute("UPDATE picture SET is_vectorized=1 WHERE id=%s", (pid,))
